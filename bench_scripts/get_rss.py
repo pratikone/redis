b7940b662911655ae6429e21d4c4321587e1c1cc
Ali Anwar <apassi@live.com>
2016-11-19 16:20:47 -0500

    added benchmark scripts; tweak it for your usage

---
+++ bench_scripts/get_rss.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import psutil, sys, getopt
+
+def fetch_rss(proc_name):
+	for proc in psutil.process_iter():
+		#pinfo = proc.as_dict(attrs=['pid', 'name'])
+		#print proc.name()
+		if proc.name() == proc_name:
+			print proc.memory_info()
+
+if __name__ == "__main__":
+	binary = ''
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], "hb:")
+	except getopt.GetoptError:
+		print 'test.py -b <binary_name>'
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt == '-h':
+			print 'test.py -b <binary_name>'
+		elif opt == '-b':
+			binary = arg
+	
+	fetch_rss(binary)
